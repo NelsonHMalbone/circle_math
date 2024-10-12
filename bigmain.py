381a6121b37a48cebbeb48d03aff1c518a1479fb
Nelson Malbone <nhmalbone@icloud.com>
2024-10-12 07:20:16 -0400

    10-11-2024 added in the pie math to get the area of radius and diameters

--- bigmain.py
+++ bigmain.py
@@ -7,11 +7,15 @@ d_r_input = input('Enter "D" for Diameter or "R" for Radius: ')
 if d_r_input == "D":
     diameter_results = int(input("Enter Radius here: "))
     results = diameter_results * 2
-    print(results)
+    #print(f'The diameter is {results}')
+    pie_results = (3.14/2) * diameter_results ** 2
+    print(f'The diameter is {results} and the area is {pie_results} m2')
 elif d_r_input == "R":
     radius_results = int(input("Enter diameter here: "))
     results = radius_results / 2
-    print(results)
+    #print(f'The radius is {results}')
+    pie_results = 3.14 * results ** 2
+    print(f'The radius is {results} and the area is {pie_results} m2')
 else:
     print("Besure to use captial D or R")
 
